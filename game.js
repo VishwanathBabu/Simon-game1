847a983dbe882989b902e453b31c0dbaf4fee158
VishwanathBabu <vishwanathbabu1799@gmail.com>
2024-06-23 20:02:10 +0530

    Update game.js

--- game.js
+++ game.js
@@ -63,7 +63,7 @@ function nextsequence(){
     }
 
 function playsound(name){
-        var audio = new Audio("sounds/" + name + ".mp3");
+        var audio = new Audio(name + ".mp3");
         audio.play();  
     }
 
